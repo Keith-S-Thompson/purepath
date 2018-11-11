81324df6d8b7c95028becd0de6d5878e23d59675
Keith Thompson <Keith.S.Thompson@gmail.com>
2018-11-11 14:37:54 -0800

    Accept path on command line, cleaner malloc failure handling

--- purepath.c
+++ purepath.c
@@ -1,18 +1,43 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-int main(void) {
-    const char *path = getenv("PATH");
-    if (path == NULL) {
-        fprintf(stderr, "$PATH is not set\n");
-        exit(EXIT_FAILURE);
+
+static void usage_error(char *program_name) {
+    fprintf(stderr, "Usage: %s [path]\n", program_name);
+    exit(EXIT_FAILURE);
+}
+
+static void malloc_failure(char *message) {
+    fprintf(stderr, "malloc failed for %s\n", message);
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char **argv) {
+    const char *path;
+    switch (argc) {
+        case 1:
+            path = getenv("PATH");
+            if (path == NULL) {
+                fprintf(stderr, "$PATH is not set\n");
+                usage_error(argv[0]);
+            }
+            break;
+        case 2:
+            path = argv[1];
+            if (path[0] == '-') {
+                usage_error(argv[0]);
+            }
+            break;
+        default:
+            usage_error(argv[0]);
+            break;
     }
 
     const size_t pathlen = strlen(path);
 
     char *const path2 = malloc(pathlen + 1);
     if (path2 == NULL) {
-        exit(EXIT_FAILURE);
+        malloc_failure("path2");
     }
     strcpy(path2, path);
 
@@ -26,7 +51,7 @@ int main(void) {
     char **elements = malloc(element_count * sizeof *elements);
     if (elements == NULL) {
         free(path2);
-        exit(EXIT_FAILURE);
+        malloc_failure("elements");
     }
     int elem_index = 0;
     for (size_t i = 0; i < pathlen; i ++) {
