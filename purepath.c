19bd7e04881ad3354b4604a94f8c56ed8c74f5f8
Keith Thompson <Keith.S.Thompson@gmail.com>
2018-11-11 19:30:26 -0800

    Bug fix: Handle trailing : and consecutive ::

--- purepath.c
+++ purepath.c
@@ -42,10 +42,10 @@ int main(int argc, char **argv) {
     strcpy(path2, path);
 
     size_t element_count = 1;
-    for (char *p = path2; *p != '\0'; p ++) {
-        if (*p == ':') {
+    for (size_t i = 0; i < pathlen; i ++) {
+        if (path2[i] == ':') {
             element_count ++;
-            *p = '\0';
+            path2[i] = '\0';
         }
     }
     char **elements = malloc(element_count * sizeof *elements);
@@ -54,7 +54,7 @@ int main(int argc, char **argv) {
         malloc_failure("elements");
     }
     int elem_index = 0;
-    for (size_t i = 0; i < pathlen; i ++) {
+    for (size_t i = 0; i <= pathlen; i ++) {
         if (i == 0 || path2[i-1] == '\0') {
             elements[elem_index++] = path2+i;
         }
@@ -62,7 +62,7 @@ int main(int argc, char **argv) {
 
     for (size_t i = 0; i < element_count; i ++) {
         for (size_t j = 0; j < i; j ++) {
-            if (strcmp(elements[i], elements[j]) == 0) {
+            if (elements[j] != NULL && strcmp(elements[i], elements[j]) == 0) {
                 elements[i] = NULL;
                 break;
             }
